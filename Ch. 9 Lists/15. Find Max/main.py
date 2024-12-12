7eea6758392c7f2bca143e7ff4d2f995731d1a8b
Mihail Stamenov <mihail.stamenov@dxc.com>
2024-12-13 00:18:29 +0200

    Add Ch 9: Lists, 15: Find Max

---
+++ Ch. 9 Lists/15. Find Max/main.py
@@ -0,0 +1,9 @@
+def find_max(nums):
+    max_so_far = float("-inf")
+    for num in nums:   
+        if num == max_so_far:
+            max_so_far = float("-inf")
+            return max_so_far
+        if num > max_so_far:
+            max_so_far = num
+    return max_so_far
