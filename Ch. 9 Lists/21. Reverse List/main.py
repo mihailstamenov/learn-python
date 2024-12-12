7bb2ad32861d3a8bfa01e8dac3d4926ac5d51513
Mihail Stamenov <mihail.stamenov@dxc.com>
2024-12-13 00:20:11 +0200

    Add Ch 9: Lists, 21: Reverse List

---
+++ Ch. 9 Lists/21. Reverse List/main.py
@@ -0,0 +1,15 @@
+# def reverse_array(items):
+
+items = ["Shortsword", "Healing Potion", "Iron Breastplate", "Kite Shield"]
+reversed = []
+# for i in range(1, len(items)+1):
+for i in range(len(items)-1, -1, -1):
+    print(i)
+
+print(reversed)
+
+# def reverse_array(items):
+#     new_array = []
+#     for i in range(len(items) - 1, -1, -1):
+#         new_array.append(items[i])
+#     return new_array
