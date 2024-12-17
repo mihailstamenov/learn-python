5fa15c04cd59f512caf2fada5f03573f5d61d823
Mihail Stamenov <michaelstamenov911@gmail.com>
2024-12-18 01:33:49 +0200

    Add Ch 12: Errors, 7: Errors and exceptions in Python

---
+++ Ch. 12 Errors/07. Different types of exceptions/main.py
@@ -0,0 +1,22 @@
+def handle_get_player_record(player_id):
+    try:
+        res = get_player_record(player_id)
+    except IndexError:
+        return "index is too high"
+    except Exception as e:
+        return e
+    return res
+
+
+# Don't edit below this line
+
+
+def get_player_record(player_id):
+    if player_id < 0:
+        raise Exception("negative ids not allowed")
+    players = [
+        {"name": "Slayer", "level": 128},
+        {"name": "Dorgoth", "level": 300},
+        {"name": "Saruman", "level": 4000},
+    ]
+    return players[player_id]
