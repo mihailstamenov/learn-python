994897d69cb4740257f9e541aeb710e6d5e5c102
Mihail Stamenov <michaelstamenov911@gmail.com>
2024-12-19 22:58:44 +0200

    Add Ch 13: Challenges, 6: FizzBuzz

---
+++ Ch. 13 Challenges/06. FizzBuzz/main.py
@@ -0,0 +1,29 @@
+def fizzbuzz(start, end):
+    for i in range(start, end-1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 5 == 0: 
+            print("buzz")
+        elif i % 3 == 0: 
+            print("fizz")
+        else:
+            print(i)
+
+
+
+# Don't Touch Below This Line
+
+
+def main():
+    test(1, 100)
+    test(5, 30)
+    test(1, 15)
+
+
+def test(start, end):
+    print("Starting test")
+    fizzbuzz(start, end)
+    print("======================")
+
+
+main()
