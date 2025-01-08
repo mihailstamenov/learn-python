40ca1c57c8a236f68cfff26c7a45c143120dc853
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-08 17:57:14 +0200

    Add Ch 2: Variables, 8: Basic Variable Types

---
+++ Ch. 2 Variables/08. Basic Variable Types.py
@@ -0,0 +1,7 @@
+player_health = "100"
+
+player_has_magic = "True"
+
+# don't touch below this line
+print(f"player_health is a/an {type(player_health).__name__}")
+print(f"player_has_magic is a/an {type(player_has_magic).__name__}")
