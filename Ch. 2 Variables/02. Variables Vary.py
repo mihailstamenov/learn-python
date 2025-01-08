f0f4def0647e62b14072516ded44a641e0916d28
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-08 17:49:23 +0200

    Add Ch 2: Variables, 2: Variables Vary

---
+++ Ch. 2 Variables/02. Variables Vary.py
@@ -0,0 +1,17 @@
+player_health = 1000
+
+# reduce by 100 here
+
+print(player_health)
+
+# and here
+
+print(player_health)
+
+# and here
+
+print(player_health)
+
+# and here
+
+print(player_health)
