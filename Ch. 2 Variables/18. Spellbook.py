7c6ceb0cc9da3487361b247f4b062a6b6edacc11
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-09 22:44:18 +0200

    Add Ch 2: Variables, 18: Spellbook

---
+++ Ch. 2 Variables/18. Spellbook.py
@@ -0,0 +1,12 @@
+game_one_score = 97
+game_two_score = 91
+game_three_score = 106
+game_four_score = 105
+
+# Don't touch above this line
+
+average_score = # ?
+
+# Don't touch below this line
+
+print(round(average_score))
