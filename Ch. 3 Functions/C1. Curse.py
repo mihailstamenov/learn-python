dd762b696663b5b033dc7c7c0a1e98f1f120824c
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-09 22:54:11 +0200

    Add Ch 3: Functions, C1: Curse

---
+++ Ch. 3 Functions/C1. Curse.py
@@ -0,0 +1,24 @@
+def curse(weapon_damage):
+    lesser_cursed = weapon_damage/2
+    greater_cursed = weapon_damage/4
+    return lesser_cursed, greater_cursed
+
+# Don't modify below this line
+
+
+def test(weapon_damage):
+    print("Weapon's base damage:", float(weapon_damage))
+    print("Cursing...")
+    lesser_cursed, greater_cursed = curse(weapon_damage)
+    print("With lesser curse the damage is:", float(lesser_cursed), "damage.")
+    print("With greater curse the damage is:", float(greater_cursed), "damage.")
+    print("=====================================")
+
+
+def main():
+    test(100)
+    test(500)
+    test(1000)
+
+
+main()
