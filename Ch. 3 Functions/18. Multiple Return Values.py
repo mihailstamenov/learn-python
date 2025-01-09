a225583203ab01dd7a7c4e06436539ac6f0e9861
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-09 22:53:19 +0200

    Add Ch 3: Functions, 18: Multiple Return Values

---
+++ Ch. 3 Functions/18. Multiple Return Values.py
@@ -0,0 +1,20 @@
+def become_warrior(full_name, power):
+    # ?
+
+
+# Don't edit below this line
+
+
+def main():
+    test("Frodo Baggins", 5)
+    test("Bilbo Baggins", 10)
+    test("Gandalf The Grey", 9000)
+
+
+def test(input1, input2):
+    result1, result2 = become_warrior(input1, input2)
+    print(result1, "has a power level of:", result2)
+
+
+main()
+
