1a461bfe8f6d2fdb0e8aa17fd4b7d6bc0791f790
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-09 22:32:33 +0200

    Add Ch 2: Variables, 17: Boots

---
+++ Ch. 2 Variables/17. Boots.py
@@ -0,0 +1,7 @@
+quest_start = "You there! Adventurer!"
+quest_middle = "The local mine has been taken over by orcs!"
+quest_end = "We need your help taking it back."
+quest_objective = "Bring back 8 of their axes as proof of your hard work."
+space = " "
+
+# don't touch above this line
