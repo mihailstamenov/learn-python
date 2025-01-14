ac883df089139f1f4a2db389b59a4ed8a833760d
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-14 19:27:36 +0200

    Add Ch 7: Comparisons, 7: If-Else Practice

---
+++ Ch. 7 Comparisons/07. If-Else Practice/main.py
@@ -0,0 +1,5 @@
+def check_high_score(current_player_name, high_scoring_player_name):
+    if True:
+        return "You are the highest scoring player!"
+    else:
+        return "You are not the highest scoring player!"
