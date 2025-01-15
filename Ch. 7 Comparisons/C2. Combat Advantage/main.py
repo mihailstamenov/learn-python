d24fe9be1e7f1de687a1c744026876c87ee41607
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-15 20:55:36 +0200

    Add Ch 7: Comparisons, C2: Combat Advantage

---
+++ Ch. 7 Comparisons/C2. Combat Advantage/main.py
@@ -0,0 +1,6 @@
+def combat_evaluation(player_power, enemy_defense):
+    advantage, disadvantage, evenly_matched = False, False, False
+
+    # your code here
+
+    return advantage, disadvantage, evenly_matched
