8943b85cbd2e153a691751e781b09bb431d4b85d
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-15 20:56:11 +0200

    Add Ch 8: Loops, 1: Loops

---
+++ Ch. 8 Loops/01. Loops/main.py
@@ -0,0 +1,19 @@
+def print_numbers():
+    for :
+        print(i)
+
+
+# Don't edit below this line
+
+
+def test():
+    print("Printing numbers from 0 to 99:")
+    print_numbers()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
