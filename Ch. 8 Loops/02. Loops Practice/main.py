cc232a3d1a30d0941066d617c11544bf0af9d140
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-15 20:56:29 +0200

    Add Ch 8: Loops, 2: Loops Practice

---
+++ Ch. 8 Loops/02. Loops Practice/main.py
@@ -0,0 +1,18 @@
+def print_numbers():
+    # ?
+
+
+# Don't edit below this line
+
+
+def test():
+    print("Printing numbers from 0 to 199:")
+    print_numbers()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
